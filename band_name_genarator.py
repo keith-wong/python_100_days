fe5302e8b250c7e30d4c836d14b525be9963040c
KeithWong <kwongchiu66@gmail.com>
2024-10-26 15:10:35 +0800

    edit README.md and band_name_generator.py

---
+++ band_name_genarator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator?");
+city=input("Where is the city you are living in?\n");
+print(f"You are living in {city}");
+pet_name=input("What is the name of your pet?\n");
+print(f"The name of your pet is {pet_name}.");
+print(f"Your rock band name is {city} {pet_name}");
+
+
